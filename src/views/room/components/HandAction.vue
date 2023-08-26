9311f936d17bda2b7a28f8dacf8b0c5843ecb449
Siratee K <sirateek@gmail.com>
2023-08-26 22:29:26 +0700

    feat: Add pick index logic

--- src/views/room/components/HandAction.vue
+++ src/views/room/components/HandAction.vue
@@ -11,9 +11,10 @@
       class="grid grid-rows-1 grid-flow-col gap-4 w-full h-full overflow-x-scroll pt-4"
     >
       <div
-        class="w-20 hover:-translate-y-4 duration-150"
+        :class="cardClass(i)"
         v-for="i in 20"
         :key="i"
+        @click="updatePickedIndex(i)"
       >
         <Card value="10" text-size="text-lg"></Card>
       </div>
@@ -24,11 +25,34 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Card from "@/components/Card.vue";
+import { reactive } from "vue";
 
 export default defineComponent({
   name: "HandAction",
   components: {
     Card,
+  },
+  setup() {
+    const attributes = reactive({
+      pickedIndex: -1,
+    });
+
+    const updatePickedIndex = (pickedIndex: number) => {
+      attributes.pickedIndex = pickedIndex;
+    };
+
+    const cardClass = (pickedIndex: number) => {
+      if (pickedIndex != attributes.pickedIndex) {
+        return "w-20 hover:-translate-y-4 duration-150";
+      }
+
+      return "w-20 -translate-y-4 duration-150";
+    };
+
+    return {
+      cardClass,
+      updatePickedIndex,
+    };
   },
 });
 </script>
