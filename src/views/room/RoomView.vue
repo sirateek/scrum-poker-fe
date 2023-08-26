465f459ae6147fcc530884959ee9bff475777fcb
Siratee K <sirateek@gmail.com>
2023-08-26 22:12:33 +0700

    feat: Extract InformationBoard

--- src/views/room/RoomView.vue
+++ src/views/room/RoomView.vue
@@ -6,48 +6,38 @@
         class="grid grid-cols-[repeat(auto-fit,14%)] justify-center items-center w-full gap-4 overflow-y-scroll"
       >
         <CardWithName
-          v-for="i in 1"
+          v-for="i in 20"
           :key="i"
-          value="3"
+          :value="i.toString()"
           name="TonnamTest"
         ></CardWithName>
       </div>
-      <div v-if="true" class="text-center gap-5 flex flex-col w-[15vw]">
-        <VCard class="p-3 rounded-md">
-          <h2>Score Board</h2>
-          <div class="flex flex-col gap-2 mt-3 max-h-[65vh] overflow-y-scroll">
-            <div v-for="i in 20" :key="i">
-              <div class="flex flex-row">
-                <Card value="2" text-size="text-md"></Card>
-                <p class="ml-1 flex items-center justify-center">x 5</p>
-              </div>
-              <VDivider class="mt-2"></VDivider>
-            </div>
+      <div class="flex flex-row gap-2">
+        <InformationBoard title="Score Board">
+          <div v-for="i in 20" :key="i">
             <div class="flex flex-row">
-              <Card value="?" text-size="text-md"></Card>
-              <p class="ml-1 flex items-center justify-center">x 5</p>
+              <Card :value="i.toString()" text-size="text-md"></Card>
+              <p class="ml-1 flex items-center justify-center">x {{ i }}</p>
             </div>
             <VDivider class="mt-2"></VDivider>
           </div>
-        </VCard>
-      </div>
-
-      <div class="text-center gap-5 flex flex-col w-[18vw] ml-3">
-        <VCard class="p-3 rounded-md">
-          <h2>Player</h2>
-          <div
-            class="flex flex-col items-start max-h-[65vh] gap-2 mt-3 overflow-y-scroll"
-          >
-            <div>
-              <VIcon icon="mdi-eye"></VIcon>
-              Spectator x 5
-            </div>
-            <div v-for="i in 30" :key="i">
-              <VIcon icon="mdi-account"></VIcon>
-              Tonnam
-            </div>
+          <div class="flex flex-row">
+            <Card value="?" text-size="text-md"></Card>
+            <p class="ml-1 flex items-center justify-center">x 5</p>
           </div>
-        </VCard>
+          <VDivider class="mt-2"></VDivider>
+        </InformationBoard>
+
+        <InformationBoard title="Player">
+          <div>
+            <VIcon icon="mdi-eye"></VIcon>
+            Spectator x 5
+          </div>
+          <div v-for="i in 30" :key="i">
+            <VIcon icon="mdi-account"></VIcon>
+            Tonnam
+          </div>
+        </InformationBoard>
       </div>
     </div>
 
@@ -63,6 +53,7 @@ import { defineComponent } from "vue";
 import CardWithName from "./components/CardWithName.vue";
 import Card from "@/components/Card.vue";
 import HandAction from "@/views/room/components/HandAction.vue";
+import InformationBoard from "./components/InformationBoard.vue";
 
 export default defineComponent({
   name: "RoomView",
@@ -70,6 +61,7 @@ export default defineComponent({
     Card,
     HandAction,
     CardWithName,
+    InformationBoard,
   },
 });
 </script>
---
+++ src/views/room/components/InformationBoard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center gap-5 flex flex-col w-[15vw]">
+    <VCard class="p-3 rounded-md">
+      <h2>{{ title }}</h2>
+      <div class="flex flex-col gap-2 mt-3 max-h-[60vh] overflow-y-scroll">
+        <slot></slot>
+      </div>
+    </VCard>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InformationBoard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
