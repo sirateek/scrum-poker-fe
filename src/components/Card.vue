9905ce2280e91c342a21fcd8ac5386d44aec1e49
Siratee K <sirateek@gmail.com>
2023-08-26 16:22:10 +0700

    feat: Update

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,19 +2,20 @@
   <div class="relative aspect-[1/1.5] max-w-[130px] max-h-[200px]">
     <VCard color="#5C8984" class="rounded-xl h-full w-full z-10">
       <div class="flex items-center justify-center h-full w-full p-3">
-        <p class="text-3xl truncate">
+        <p :class="textClasses">
           {{ value }}
         </p>
       </div>
     </VCard>
-    <div class="pt-7">
+    <div v-if="hasSlot('below')" class="pt-7">
       <slot name="below"></slot>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { reactive } from "vue";
+import { useSlots } from "vue";
+import { computed } from "vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -24,12 +25,25 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    textSize: {
+      type: String,
+      default: "text-3xl",
+    },
   },
-  setup() {
-    const attributes = reactive({
-      maxWidth: "130px",
-      maxHeight: "182px",
+  setup(props) {
+    const textClasses = computed(() => {
+      return `${props.textSize} truncate`;
     });
+
+    const slots = useSlots();
+    const hasSlot = (name: string | number) => {
+      return !!slots[name];
+    };
+
+    return {
+      textClasses,
+      hasSlot,
+    };
   },
 });
 </script>
--- src/views/room/RoomView.vue
+++ src/views/room/RoomView.vue
@@ -1,23 +1,79 @@
 <template>
   <div class="mx-8">
-    <div class="grid grid-cols-8 gap-4">
-      <CardWithName
-        v-for="i in 10"
-        :key="i"
-        value="3"
-        name="TonnamTest"
-      ></CardWithName>
+    <!-- Display -->
+    <div class="flex flex-row max-h-[55vh]">
+      <div class="grid grid-cols-5 w-full gap-4 overflow-y-scroll">
+        <CardWithName
+          v-for="i in 20"
+          :key="i"
+          value="3"
+          name="TonnamTest"
+        ></CardWithName>
+      </div>
+      <div v-if="false" class="text-center gap-5 flex flex-col w-[15vw]">
+        <VCard class="p-3 rounded-md">
+          <h2>Score Board</h2>
+          <div class="flex flex-col gap-2 mt-3 max-h-[49vh] overflow-y-scroll">
+            <div v-for="i in 7" :key="i">
+              <div class="flex flex-row">
+                <Card value="2" text-size="text-md"></Card>
+                <p class="ml-1 flex items-center justify-center">x 5</p>
+              </div>
+              <VDivider class="mt-2"></VDivider>
+            </div>
+            <div class="flex flex-row">
+              <Card value="?" text-size="text-md"></Card>
+              <p class="ml-1 flex items-center justify-center">x 5</p>
+            </div>
+            <VDivider class="mt-2"></VDivider>
+          </div>
+        </VCard>
+      </div>
+
+      <div class="text-center gap-5 flex flex-col w-[18vw] ml-3">
+        <VCard class="p-3 rounded-md">
+          <h2>Player</h2>
+          <div
+            class="flex flex-col items-start max-h-[49vh] gap-2 mt-3 overflow-y-scroll"
+          >
+            <div>
+              <VIcon icon="mdi-eye"></VIcon>
+              Spectator x 5
+            </div>
+            <div v-for="i in 30" :key="i">
+              <VIcon icon="mdi-account"></VIcon>
+              Tonnam
+            </div>
+          </div>
+        </VCard>
+      </div>
+    </div>
+
+    <!-- Hand -->
+    <div class="max-h-[40vh]">
+      <!-- Card Selection -->
+      <div class="grid grid-rows-1 grid-flow-col gap-4 w-full h-full">
+        <div
+          class="w-20 hover:bg-sky-700 duration-150"
+          v-for="i in 11"
+          :key="i"
+        >
+          <Card value="1"></Card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
+≈
 <script lang="ts">
 import { defineComponent } from "vue";
 import CardWithName from "./components/CardWithName.vue";
+import Card from "@/components/Card.vue";
 
 export default defineComponent({
   name: "RoomView",
   components: {
+    Card,
     CardWithName,
   },
 });
